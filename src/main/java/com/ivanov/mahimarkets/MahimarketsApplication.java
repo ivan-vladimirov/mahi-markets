1c6ba20d753f790717169aa7d8638a726eba5ee8
Ivanov, Ivan (UG - Computer Science) <ii00088@surrey.ac.uk>
2022-08-15 17:07:23 +0100

    initial push

---
+++ src/main/java/com/ivanov/mahimarkets/MahimarketsApplication.java
@@ -0,0 +1,13 @@
+package com.ivanov.mahimarkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahimarketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahimarketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanov/mahimarkets/MahimarketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanov.mahimarkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahimarketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
